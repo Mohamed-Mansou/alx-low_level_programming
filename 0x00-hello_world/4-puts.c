9d63048b5badedfddba4038ff06696727b1810ba
Mohamed-Mansou <moyamed2mansour@gmail.com>
2023-02-16 09:58:58 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,10 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+ *    
+ */
 int main(void)
-{    puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+{  
+   	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
