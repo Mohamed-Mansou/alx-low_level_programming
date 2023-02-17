eed02346d65dd1a14137711e8a749ce860f11299
Mohamed-Mansou <moyamed2mansour@gmail.com>
2023-02-17 10:06:07 -0800

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase.
+(*
+* Return: 0 on success
+*/
+int main(void)
+{
+  char c;
+
+  for (c = 'z'; c >= 'a'; c--)
+    {
+      putchar(c);
+    }
+  putchar('\n');
+
+  return (0);
+}
