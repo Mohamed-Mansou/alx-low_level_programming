f2a2e51299574f2017da3cc4b16bd4273317cfab
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-02-17 21:28:53 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * Return: always 0 (success)
- */
+* main - entry point
+* Return: always 0 (success)
+*/
 int main(void)
 {
-  int n;
+int n;
 
-  n = 48;
+n = 48;
 
-  while (n <= 57)
-    {
-      putchar (n);
-      if (n != 57)
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-      n++;
-    }
-  putchar ('\n');
-  return (0);
+while (n <= 57)
+{
+putchar (n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
+n++;
+}
+putchar ('\n');
+return (0);
 }
