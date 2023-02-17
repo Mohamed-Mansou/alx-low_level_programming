8f6ffcbd98d2e3a96f05fde1b166678826a7b68d
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-02-17 21:21:42 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints the numbers from 0-9.
+* main - Prints the numbers from 0-9.
 (*
 * Return: 0 on success
 */
 int main(void)
 {
-  printf("0123456789\n");
-  return (0);
+printf("0123456789\n");
+return (0);
 }
