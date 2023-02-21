e40fbed2a82ccfe069fa97a69375320ccd1e8792
Mohamed-Mansou <moyamed2mansour@gmail.com>
2023-02-21 14:50:41 -0800

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main (void)
+{
+  char destination[] = "_putchar";
+
+  int i = 0;
+  while (destination[i] != '\0')
+    {
+      _putchar(destination[i]);
+      i++;
+    }
+  _putchar('\n');
+  return (0);
+}
