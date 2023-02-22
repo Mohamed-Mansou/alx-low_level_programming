73f6325fca0215a9639b23874769ca89d5442978
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-02-22 03:14:52 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Lists all the natural numbers below 1024 (excluded)
- *        that are multiples of 3 or 5.
- *
- * Return: Always 0.
- */
+* main - Lists all the natural numbers below 1024 (excluded)
+*        that are multiples of 3 or 5.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-  int i, sum = 0;
-
-  for (i = 0; i < 1024; i++)
-    {
-      if ((i % 3) == 0 || (i % 5) == 0)
-	sum += i;
-    }
-
-  printf("%d\n", sum);
-
-  return (0);
+int i, sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+sum += i;
+}
+printf("%d\n", sum);
+return (0);
 }
 Footer
 © 2023 GitHub, Inc.
