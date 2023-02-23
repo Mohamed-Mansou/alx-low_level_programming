f8c07fbed3e8d24fe25e9d7a88bab4ec6cc075ca
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-02-24 02:38:14 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * *print_numbers - print 0123456789
- * * owned by Bwave ICT
- * *Return: void
- * */
+* *print_numbers - print 0123456789
+* * owned by Bwave ICT
+* *Return: void
+* */
 
 void print_numbers(void)
 {
-		char c;
-
-			for (c = '0'; c <= '9'; c++)
-					{
-							_putchar(c);
-								}
-				_putchar('\n');
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
