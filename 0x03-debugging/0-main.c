5d272c8656d38e4d32cdd167b17443ca137749c3
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-02-23 21:38:12 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -7,6 +8,6 @@ int main(void)
 {
 int i;
 i = 0;
-positive_or_negative(i);
+positive_or_negative(i);	
 return (0);
 }
