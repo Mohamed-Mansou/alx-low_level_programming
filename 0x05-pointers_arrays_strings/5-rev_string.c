1a10fc1a900ef1f8aaa7bbb1fd1af44fd51927e0
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-02-27 21:19:26 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,22 @@
 void rev_string(char *s)
 {
 
-  int len, i, half;
-  char temp;
+int len, i, half;
+char temp;
 
-  for (len = 0; s[len] != '\0'; len++)
-    ;
+for (len = 0; s[len] != '\0'; len++)
+;
 
-  i = 0;
+i = 0;
 
-  half = len / 2;
+half = len / 2;
 
-  while (half--)
-    {
-      temp = s[len - i - 1];
-      s[len - i - 1] = s[i];
-      s[i] = temp;
-      i++;
-    }
+while (half--)
+{
+temp = s[len - i - 1];
+s[len - i - 1] = s[i];
+s[i] = temp;
+i++;
+}
 
 }
