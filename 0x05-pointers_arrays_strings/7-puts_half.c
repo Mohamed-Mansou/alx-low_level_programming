a039ee665afe57d01041aead0ebe61d4c07cdf1b
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-02-27 21:30:21 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,27 +7,27 @@
 
 void puts_half(char *str)
 {
-  int len, n, i;
+int len, n, i;
 
-  len = 0;
+len = 0;
 
-  while (str[len] != '\0')
-    {
-      len++;
-    }
+while (str[len] != '\0')
+{
+len++;
+}
 
-  if (len % 2 == 0)
-    {
-      for (i = len / 2; str[i] != '\0'; i++)
-	{
-	  _putchar(str[i]);
-	}
-    } else if (len % 2)
-    {
-      for (n = (len - 1) / 2; n < len - 1; n++)
-	{
-	  _putchar(str[n + 1]);
-	}
-    }
-  _putchar('\n');
+if (len % 2 == 0)
+{
+for (i = len / 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+} else if (len % 2)
+{
+for (n = (len - 1) / 2; n < len - 1; n++)
+{
+_putchar(str[n + 1]);
+}
+}
+_putchar('\n');
 }
