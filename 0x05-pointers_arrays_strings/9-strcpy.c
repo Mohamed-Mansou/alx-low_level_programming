1dcfaea37e737e3db73c2676788c5d5ca55934f1
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-02-27 20:53:39 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-  int i = -1;
+int i = -1;
 
-  do {
-    i++;
-    dest[i] = src[i];
-  } while (src[i] != '\0');
+do {
+i++;
+dest[i] = src[i];
+} while (src[i] != '\0');
 
-  return (dest);
+return (dest);
 }
