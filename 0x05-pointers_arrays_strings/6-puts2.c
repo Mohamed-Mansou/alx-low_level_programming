e1a28cd23686c7397196b1167c411bdbef2495ae
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-02-27 21:23:41 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,16 @@
 
 void puts2(char *str)
 {
-  int i = 0;
+int i = 0;
 
-  while (str[i] != '\0')
-    {
-      if (i % 2 == 0)
-	{
-	  _putchar(str[i]);
-	}
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
 
-      i++;
-    }
-  _putchar('\n');
+i++;
+}
+_putchar('\n');
 }
