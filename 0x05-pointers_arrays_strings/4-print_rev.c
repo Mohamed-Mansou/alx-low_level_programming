8c6ecb80f06be573a79c051046b3b88086750ebd
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-02-27 21:17:27 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,16 @@
 
 void print_rev(char *s)
 {
-  int i = 0;
+int i = 0;
 
-  while (s[i])
-    i++;
+while (s[i])
+i++;
 
-  while (i--)
-    {
-      _putchar(s[i]);
-    }
+while (i--)
+{
+_putchar(s[i]);
+}
 
-  _putchar('\n');
+_putchar('\n');
 
 }
