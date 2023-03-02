7a12b02ab882b2ceb1045e5ccfeac145c1fd14bd
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-02 19:04:30 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,24 +10,24 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-  int i, j;
+int i, j;
 
-  if (n <= 0)
-    {
-      return (dest);
-    }
+if (n <= 0)
+{
+return (dest);
+}
 
-  i = 0;
-  j = 0;
+i = 0;
+j = 0;
 
-  while (dest[i] != '\0')
-    {
-      i++;
-    }
-  while (j < n && src[j] != '\0')
-    {
-      dest[i++] = src[j++];
-    }
-  dest[i++] = '\0';
-  return (dest);
+while (dest[i] != '\0')
+{
+i++;
+}
+while (j < n && src[j] != '\0')
+{
+dest[i++] = src[j++];
+}
+dest[i++] = '\0';
+return (dest);
 }
