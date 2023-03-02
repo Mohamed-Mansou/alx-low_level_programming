0df7e5ed2de06cee46f017f4df71f4d62dc3bccd
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-02 19:07:45 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,15 +8,15 @@
 
 void reverse_array(int *a, int n)
 {
-  int b;
-  int i;
-  int j = n - 1;
+int b;
+int i;
+int j = n - 1;
 
-  for (i = 0; i < j; i++, j--)
-    {
-      b = a[i];
-      a[i] = a[j];
-      a[j] = b;
-    }
+for (i = 0; i < j; i++, j--)
+{
+b = a[i];
+a[i] = a[j];
+a[j] = b;
+}
 
 }
