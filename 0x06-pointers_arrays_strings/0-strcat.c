cb29659f0485cf97b995ff078ecdd32029ebd369
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-02 18:50:25 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-  int l;
-  int i;
+int l;
+int i;
 
-  for (l = 0; dest[l] != '\0'; l++)
-    {
-    }
-  for (i = 0; src[i] != '\0'; i++)
-    {
-      dest[l + i] = src[i];
-    }
-  return (dest);
+for (l = 0; dest[l] != '\0'; l++)
+{
+}
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[l + i] = src[i];
+}
+return (dest);
 }
