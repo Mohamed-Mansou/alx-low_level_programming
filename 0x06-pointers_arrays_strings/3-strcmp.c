c701e7becfc47094169a76dc7e45add7e473c062
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-02 19:06:49 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,26 +10,26 @@
 int _strcmp(char *s1, char *s2)
 {
 
-  while (*s1 != '\0' && *s2 != '\0')
-    {
-      if (*s1 != *s2)
-	{
-	  return (*s1 - *s2);
-	}
-      else if (*s1 == '\0' || *s2 == '\0')
-	{
-	  break;
-	}
-      s1++;
-      s2++;
-    }
+while (*s1 != '\0' && *s2 != '\0')
+{
+if (*s1 != *s2)
+{
+return (*s1 - *s2);
+}
+else if (*s1 == '\0' || *s2 == '\0')
+{
+break;
+}
+s1++;
+s2++;
+}
 
-  if (*s1 == *s2)
-    {
-      return (0);
-    }
-  else
-    {
-      return (*s1 - *s2);
-    }
+if (*s1 == *s2)
+{
+return (0);
+}
+else
+{
+return (*s1 - *s2);
+}
 }
