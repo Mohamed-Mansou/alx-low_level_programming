0480ff13348040358130f85fce0422bcc9ac7b51
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-03 02:59:19 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,21 @@
 
 char *leet(char *s)
 {
-  int i, ii;
-  char s1[] = "aeotl";
-  char S1[] = "AEOTL";
-  char s2[] = "43071";
+int i, ii;
+char s1[] = "aeotl";
+char S1[] = "AEOTL";
+char s2[] = "43071";
 
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      for (ii = 0; ii < 5; ii++)
-	{
-	  if (s[i] == s1[ii] || s[i] == S1[ii])
-	    {
-	      s[i] = s2[ii];
-	      break;
-	    }
-	}
-    }
-  return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+for (ii = 0; ii < 5; ii++)
+{
+if (s[i] == s1[ii] || s[i] == S1[ii])
+{
+s[i] = s2[ii];
+break;
+}
+}
+}
+return (s);
 }
