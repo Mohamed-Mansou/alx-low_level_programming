e4e5b780d3cf849378babeb39001a23f8c54575d
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-03 02:58:02 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,36 +8,36 @@
 
 char *cap_string(char *str)
 {
-  char sep[] = ",\t;\n; .!?\"(){}";
-  int flag, i, ii;
+char sep[] = ",\t;\n; .!?\"(){}";
+int flag, i, ii;
 
-  for (i = 0; str[i] != '\0'; i++)
-    {
-      flag = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+flag = 0;
 
-      if (i == 0)
-	{
-	  flag = 1;
-	}
-      else
-	{
-	  for (ii = 0; sep[ii] != '\0'; ii++)
-	    {
-	      if (str[i - 1] == sep[ii])
-		{
-		  flag = 1;
-		  break;
-		}
-	    }
-	}
+if (i == 0)
+{
+flag = 1;
+}
+else
+{
+for (ii = 0; sep[ii] != '\0'; ii++)
+{
+if (str[i - 1] == sep[ii])
+{
+flag = 1;
+break;
+}
+}
+}
 
-      if (flag == 1)
-	{
-	  if (str[i] <= 'z' && str[i] >= 'a')
-	    {
-	      str[i] -= ('a' - 'A');
-	    }
-	}
-    }
-  return (str);
+if (flag == 1)
+{
+if (str[i] <= 'z' && str[i] >= 'a')
+{
+str[i] -= ('a' - 'A');
+}
+}
+}
+return (str);
 }
