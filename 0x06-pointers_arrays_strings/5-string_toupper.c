87dda5680afb4f7a90ff68f07c2dba6bd3773c3a
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-03 02:56:31 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,12 @@
 
 char *string_toupper(char *str)
 {
-  int i;
+int i;
 
-  for (i = 0; str[i] != '\0'; i++)
-    {
-      if (str[i] <= 'z' && str[i] >= 'a')
-	str[i] -= 32;
-    }
-  return (str);
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] <= 'z' && str[i] >= 'a')
+str[i] -= 32;
+}
+return (str);
 }
