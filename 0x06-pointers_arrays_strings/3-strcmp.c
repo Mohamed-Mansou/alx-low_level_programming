776a180f5fe62f794a0e79250cdb8b7e4c89481d
Mohamed-Mansou <moyamed2mansour@gmail.com>
2023-03-02 07:57:40 -0800

    task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that copies a string
+ * @s1: string
+ * @s2: string
+ * Return: string
+ **/
+
+int _strcmp(char *s1, char *s2)
+{
+
+  while (*s1 != '\0' && *s2 != '\0')
+    {
+      if (*s1 != *s2)
+	{
+	  return (*s1 - *s2);
+	}
+      else if (*s1 == '\0' || *s2 == '\0')
+	{
+	  break;
+	}
+      s1++;
+      s2++;
+    }
+
+  if (*s1 == *s2)
+    {
+      return (0);
+    }
+  else
+    {
+      return (*s1 - *s2);
+    }
+}
