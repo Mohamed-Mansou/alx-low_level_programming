55f61eceebfe148e546400cc978966939fa58021
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-03 03:03:02 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,22 +8,22 @@
 
 void rev_string(char *n)
 {
-  int i = 0;
-  int j = 0;
-  char temp;
+	int i = 0;
+	int j = 0;
+	char temp;
 
-  while (*(n + i) != '\0')
-    {
-      i++;
-    }
-  i--;
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
 
-  for (j = 0; j < i; j++, i--)
-    {
-      temp = *(n + j);
-      *(n + j) = *(n + i);
-      *(n + i) = temp;
-    }
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
 }
 
 /**
@@ -37,42 +37,42 @@ void rev_string(char *n)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-  int overflow = 0, i = 0, j = 0, digits = 0;
-  int val1 = 0, val2 = 0, temp_tot = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-  while (*(n1 + i) != '\0')
-    i++;
-  while (*(n2 + j) != '\0')
-    j++;
-  i--;
-  j--;
-  if (j >= size_r || i >= size_r)
-    return (0);
-  while (j >= 0 || i >= 0 || overflow == 1)
-    {
-      if (i < 0)
-	val1 = 0;
-      else
-	val1 = *(n1 + i) - '0';
-      if (j < 0)
-	val2 = 0;
-      else
-	val2 = *(n2 + j) - '0';
-      temp_tot = val1 + val2 + overflow;
-      if (temp_tot >= 10)
-	overflow = 1;
-      else
-	overflow = 0;
-      if (digits >= (size_r - 1))
-	return (0);
-      *(r + digits) = (temp_tot % 10) + '0';
-      digits++;
-      j--;
-      i--;
-    }
-  if (digits == size_r)
-    return (0);
-  *(r + digits) = '\0';
-  rev_string(r);
-  return (r);
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
