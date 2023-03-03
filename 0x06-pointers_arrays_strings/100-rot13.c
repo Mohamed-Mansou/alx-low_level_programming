d4e26f49073d13bdc85c973dffd0eac6bb8b15f6
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-03 03:06:05 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,21 +8,21 @@
 
 char *rot13(char *s)
 {
-  int i, ii;
-  char input[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int i, ii;
+char input[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      for (ii = 0; ii < 54; ii++)
-	{
-	  if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))
-	      && s[i] == input[ii])
-	    {
-	      s[i] = output[ii];
-	      break;
-	    }
-	}
-    }
-  return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+for (ii = 0; ii < 54; ii++)
+{
+if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))
+&& s[i] == input[ii])
+{
+s[i] = output[ii];
+break;
+}
+}
+}
+return (s);
 }
