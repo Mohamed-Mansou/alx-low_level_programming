aedde7d97dea23cb3dee01a5485067a845b46d6c
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-09 03:35:53 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-  int n = 0;
+int n = 0;
 
-  if (*s > '\0')
-    {
-      n += _strlen_recursion(s + 1) + 1;
-    }
+if (*s > '\0')
+{
+n += _strlen_recursion(s + 1) + 1;
+}
 
-  return (n);
+return (n);
 }
