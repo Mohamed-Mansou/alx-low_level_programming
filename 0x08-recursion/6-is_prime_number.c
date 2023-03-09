2936c04004f17fcb3bdc85583dd3541b809622b8
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-09 03:39:09 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@
 
 int is_prime_number(int n)
 {
-  return (check_prime(n, 2));
+return (check_prime(n, 2));
 }
 
 /**
@@ -22,10 +22,10 @@ int is_prime_number(int n)
 int check_prime(int n, int resp)
 {
 
-  if (resp >= n && n > 1)
-    return (1);
-  else if (n % resp == 0 || n <= 1)
-    return (0);
-  else
-    return (check_prime(n, resp + 1));
+if (resp >= n && n > 1)
+return (1);
+else if (n % resp == 0 || n <= 1)
+return (0);
+else
+return (check_prime(n, resp + 1));
 }
