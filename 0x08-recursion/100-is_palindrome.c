1375f41187620e543cc058a48546015c476f72dd
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-09 03:43:23 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,12 +8,12 @@
 
 int last_index(char *s)
 {
-  int n = 0;
+int n = 0;
 
-  if (*s > '\0')
-    n += last_index(s + 1) + 1;
+if (*s > '\0')
+n += last_index(s + 1) + 1;
 
-  return (n);
+return (n);
 }
 
 /**
@@ -24,9 +24,9 @@ int last_index(char *s)
 
 int is_palindrome(char *s)
 {
-  int end = last_index(s);
+int end = last_index(s);
 
-  return (check(s, 0, end - 1, end % 2));
+return (check(s, 0, end - 1, end % 2));
 }
 
 /**
@@ -42,10 +42,10 @@ int is_palindrome(char *s)
 int check(char *s, int start, int end, int pair)
 {
 
-  if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
-    return (1);
-  else if (s[start] != s[end])
-    return (0);
-  else
-    return (check(s, start + 1, end - 1, pair));
+if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
+return (1);
+else if (s[start] != s[end])
+return (0);
+else
+return (check(s, start + 1, end - 1, pair));
 }
