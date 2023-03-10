1f70973bc9ed2ed20f1a5caff20afb172fcf0b8d
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-10 23:24:09 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,34 +10,34 @@
 
 int main(int argc, char **argv)
 {
-  int cents, coins;
+int cents, coins;
 
-  if (argc != 2)
-    {
-      printf("Error\n");
-      return (1);
-    }
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-  cents = atoi(argv[1]);
+cents = atoi(argv[1]);
 
-  if (cents < 0)
-    {
-      printf("0\n");
-      return (0);
-    }
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
 
-  coins = 0;
-  coins += cents / 25;
-  cents %= 25;
-  coins += cents / 10;
-  cents %= 10;
-  coins += cents / 5;
-  cents %= 5;
-  coins += cents / 2;
-  cents %= 2;
-  coins += cents;
+coins = 0;
+coins += cents / 25;
+cents %= 25;
+coins += cents / 10;
+cents %= 10;
+coins += cents / 5;
+cents %= 5;
+coins += cents / 2;
+cents %= 2;
+coins += cents;
 
-  printf("%d\n", coins);
+printf("%d\n", coins);
 
-  return (0);
+return (0);
 }
