49e68538febebb4830624658bc648c2aa5a5e030
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-10 23:25:13 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,15 @@
  **/
 int main(int argc, char *argv[])
 {
-  int i, mul = 1;
+int i, mul = 1;
 
-  if (argc != 3)
-    {
-      printf("Error\n");
-      return (1);
-    }
-  for (i = 1; i < argc; i++)
-    mul *= atoi(argv[i]);
-  printf("%d\n", mul);
-  return (0);
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+for (i = 1; i < argc; i++)
+mul *= atoi(argv[i]);
+printf("%d\n", mul);
+return (0);
 }
