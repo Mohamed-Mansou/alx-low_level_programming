0cb6fcd582cfe80d21157b73de0b137101050d98
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-10 23:24:35 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,9 +9,9 @@
  **/
 int main(int argc, char *argv[])
 {
-  int i;
+int i;
 
-  for (i = 0; i < argc; i++)
-    printf("%s\n", argv[i]);
-  return (0);
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
 }
