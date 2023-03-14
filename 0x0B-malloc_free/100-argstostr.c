3f8b304b40c25ea3cd165bc9c98f18d87d325490
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-15 02:39:47 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,34 +10,34 @@
 
 char *argstostr(int ac, char **av)
 {
-  int i, n, k = 0, len = 0;
-  char *str;
+int i, n, k = 0, len = 0;
+char *str;
 
-  if (ac == 0 || av == NULL)
-    return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-  for (i = 0; i < ac; i++)
-    {
-      for (n = 0; av[i][n]; n++)
-	len++;
-    }
-  len += ac;
+for (i = 0; i < ac; i++)
+{
+for (n = 0; av[i][n]; n++)
+len++;
+}
+len += ac;
 
-  str = malloc(sizeof(char) * len + 1);
-  if (str == NULL)
-    return (NULL);
+str = malloc(sizeof(char) * len + 1);
+if (str == NULL)
+return (NULL);
 
-  for (i = 0; i < ac; i++)
-    {
-      for (n = 0; av[i][n]; n++)
-	{
-	  str[k] = av[i][n];
-	  k++;
-	}
-      if (str[k] == '\0')
-	{
-	  str[k++] = '\n';
-	}
-    }
-  return (str);
+for (i = 0; i < ac; i++)
+{
+for (n = 0; av[i][n]; n++)
+{
+str[k] = av[i][n];
+k++;
+}
+if (str[k] == '\0')
+{
+str[k++] = '\n';
+}
+}
+return (str);
 }
