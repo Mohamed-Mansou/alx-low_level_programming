e87e85f27fdce7febf44cc7ee7c0d813c12d0b6e
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-15 02:43:04 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,21 +8,21 @@
  */
 void get_words_count(char *str, int *count)
 {
-  int i, j;
-  char prev_char = ' ';
+int i, j;
+char prev_char = ' ';
 
-  *count = 0;
-  for (i = 0; str != NULL && *(str + i) != '\0'; i++)
-    {
-      if (*(str + i) != ' ' && prev_char == ' ')
-	{
-	  *count += 1;
-	  j = 0;
-	}
-      if (*(str + i) != ' ')
-	j++;
-      prev_char = *(str + i);
-    }
+*count = 0;
+for (i = 0; str != NULL && *(str + i) != '\0'; i++)
+{
+if (*(str + i) != ' ' && prev_char == ' ')
+{
+*count += 1;
+j = 0;
+}
+if (*(str + i) != ' ')
+j++;
+prev_char = *(str + i);
+}
 }
 
 /**
@@ -32,11 +32,11 @@ void get_words_count(char *str, int *count)
  */
 void get_word_length(char *str, int *length)
 {
-  *length = 0;
-  if (str == NULL || (str != NULL && (*str == '\0' || *str == ' ')))
-    return;
-  while (*(str + *length) != '\0' && *(str + *length) != ' ')
-    *length += 1;
+*length = 0;
+if (str == NULL || (str != NULL && (*str == '\0' || *str == ' ')))
+return;
+while (*(str + *length) != '\0' && *(str + *length) != ' ')
+*length += 1;
 }
 
 /**
@@ -48,41 +48,41 @@ void get_word_length(char *str, int *length)
  */
 char **strtow(char *str)
 {
-  char **words;
-  int i, j, k, len, words_count;
-  char prev_char = ' ';
+char **words;
+int i, j, k, len, words_count;
+char prev_char = ' ';
 
-  get_words_count(str, &words_count);
-  if (str == NULL || *str == '\0' || words_count == 0)
-    return (NULL);
-  words = malloc((sizeof(char *) * (words_count + 1)));
-  if (words)
-    {
-      j = -1;
-      k = 0;
-      prev_char = ' ';
-      for (i = 0; *(str + i) != '\0'; i++)
-	{
-	  if (*(str + i) != ' ')
-	    {
-	      if (prev_char == ' ')
-		{
-		  j++;
-		  get_word_length(str + i, &len);
-		  *(words + j) = malloc((sizeof(char) * (len + 1)));
-		  if (*(words + j) == NULL)
-		    return (NULL);
-		  k = 0;
-		}
-	      *(*(words + j) + k) = *(str + i);
-	      k++;
-	      if (*(str + i + 1) == '\0' || *(str + i + 1) == ' ')
-		*(*(words + j) + k) = '\0';
-	    }
-	  prev_char = *(str + i);
-	}
-      *(words + j + 1) = NULL;
-      return (words);
-    }
-  return (NULL);
+get_words_count(str, &words_count);
+if (str == NULL || *str == '\0' || words_count == 0)
+return (NULL);
+words = malloc((sizeof(char *) * (words_count + 1)));
+if (words)
+{
+j = -1;
+k = 0;
+prev_char = ' ';
+for (i = 0; *(str + i) != '\0'; i++)
+{
+if (*(str + i) != ' ')
+{
+if (prev_char == ' ')
+{
+j++;
+get_word_length(str + i, &len);
+*(words + j) = malloc((sizeof(char) * (len + 1)));
+if (*(words + j) == NULL)
+return (NULL);
+k = 0;
+}
+*(*(words + j) + k) = *(str + i);
+k++;
+if (*(str + i + 1) == '\0' || *(str + i + 1) == ' ')
+*(*(words + j) + k) = '\0';
+}
+prev_char = *(str + i);
+}
+*(words + j + 1) = NULL;
+return (words);
+}
+return (NULL);
 }
