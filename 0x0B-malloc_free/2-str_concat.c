62010daa2ab47420dea5f3e3251ca1d3d17db770
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-15 02:01:12 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,26 +10,26 @@
  **/
 char *str_concat(char *s1, char *s2)
 {
-  char *result;
-  if (s1 == NULL)
-    {
-      s1 = "";
-    }
+char *result;
+if (s1 == NULL)
+{
+s1 = "";
+}
 
-  if (s2 == NULL)
-    {
-      s2 = "";
-    }
+if (s2 == NULL)
+{
+s2 = "";
+}
 
-  result = malloc(strlen(s1) + strlen(s2) + 1);
+result = malloc(strlen(s1) + strlen(s2) + 1);
 
-  if (result == NULL)
-    {
-      return (NULL);
-    }
+if (result == NULL)
+{
+return (NULL);
+}
 
-  strcpy(result, s1);
-  strcat(result, s2);
+strcpy(result, s1);
+strcat(result, s2);
 
-  return (result);
+return (result);
 }
