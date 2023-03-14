025fd46bb979280b0fc65b72476860356a95936f
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-15 02:35:08 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,37 +11,37 @@
  **/
 int **alloc_grid(int width, int height)
 {
-  int **grid = NULL;
-  int i, j;
+int **grid = NULL;
+int i, j;
 
-  if (width <= 0 || height <= 0)
-    {
-      return (NULL);
-    }
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
 
-  grid = (int **) malloc(height * sizeof(int *));
-  if (grid == NULL)
-    {
-      return (NULL);
-    }
+grid = (int **) malloc(height * sizeof(int *));
+if (grid == NULL)
+{
+return (NULL);
+}
 
-  for (i = 0; i < height; i++)
-    {
-      grid[i] = (int *) malloc(width * sizeof(int));
-      if (grid[i] == NULL)
-	{
-	  for (j = 0; j < i; j++)
-	    {
-	      free(grid[j]);
-	    }
-	  free(grid);
-	  return (NULL);
-	}
-      for (j = 0; j < width; j++)
-	{
-	  grid[i][j] = 0;
-	}
-    }
+for (i = 0; i < height; i++)
+{
+grid[i] = (int *) malloc(width * sizeof(int));
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+{
+free(grid[j]);
+}
+free(grid);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+{
+grid[i][j] = 0;
+}
+}
 
-  return (grid);
+return (grid);
 }
