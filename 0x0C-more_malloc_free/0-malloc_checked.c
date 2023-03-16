9280110c670202b1023b87f12b3c8cc14ec7cb28
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-17 01:47:57 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,10 +7,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-  void *ptr;
+void *ptr;
 
-  ptr = malloc(b);
-  if (ptr == NULL)
-    exit(98);
-  return (ptr);
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
 }
