e3f316796304be889a0bebcba0c8f40e2a38e461
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-17 01:50:20 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,13 +9,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int i;
+unsigned int i;
 
-  for (i = 0; i < n; i++)
-    {
-      s[i] = b;
-    }
-  return (s);
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
 }
 
 /**
@@ -26,14 +26,14 @@ char *_memset(char *s, char b, unsigned int n)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  void *p;
+void *p;
 
-  if (nmemb == 0 || size == 0)
-    return (NULL);
-  p = malloc(nmemb * size);
+if (nmemb == 0 || size == 0)
+return (NULL);
+p = malloc(nmemb * size);
 
-  if (p == NULL)
-    return (NULL);
-  _memset(p, 0, (nmemb * size));
-  return (p);
+if (p == NULL)
+return (NULL);
+_memset(p, 0, (nmemb * size));
+return (p);
 }
