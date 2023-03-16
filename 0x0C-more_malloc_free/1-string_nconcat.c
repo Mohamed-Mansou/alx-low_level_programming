a632ae3336321420c9c92d76a816c2d9623c8c1f
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-17 01:49:19 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,42 +10,41 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  char *p;
-  unsigned int size1 = 0, size2 = 0, i;
+char *p;
+unsigned int size1 = 0, size2 = 0, i;
 
-  if (s1 == NULL)
-    s1 = "";
+if (s1 == NULL)
+s1 = "";
 
-  if (s2 == NULL)
-    s2 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[size1] != '\0')
+{
+size1++;
+}
 
-  while (s1[size1] != '\0')
-    {
-      size1++;
-    }
+while (s2[size2] != '\0')
+{
+size2++;
+}
 
-  while (s2[size2] != '\0')
-    {
-      size2++;
-    }
+if (n > size2)
+n = size2;
+p = malloc((size1 + n + 1) * sizeof(char));
 
-  if (n > size2)
-    n = size2;
-  p = malloc((size1 + n + 1) * sizeof(char));
+if (p == NULL)
+return (0);
 
-  if (p == NULL)
-    return (0);
+for (i = 0; i < size1; i++)
+{
+p[i] = s1[i];
+}
 
-  for (i = 0; i < size1; i++)
-    {
-      p[i] = s1[i];
-    }
+for (; i < (size1 + n); i++)
+{
+p[i] = s2[i - size1];
+}
+p[i] = '\0';
 
-  for (; i < (size1 + n); i++)
-    {
-      p[i] = s2[i - size1];
-    }
-  p[i] = '\0';
-
-  return (p);
+return (p);
 }
