5e4c399e88a216d51916711f202fc6d2f7cb30ea
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-17 01:55:16 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,25 +9,25 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-  char *clone, *relloc;
-  unsigned int i;
+char *clone, *relloc;
+unsigned int i;
 
-  if (ptr != NULL)
-    clone = ptr;
-  else
-    { return (malloc(new_size)); }
-  if (new_size == old_size)
-    return (ptr);
-  if (new_size == 0 && ptr != NULL)
-    { free(ptr);
-      return (0); }
-  relloc = malloc(new_size);
-  if (relloc == NULL)
-    return (0);
-  for (i = 0; i < (old_size || i < new_size); i++)
-    {
-      *(relloc + i) = clone[i];
-    }
-  free(ptr);
-  return (relloc);
+if (ptr != NULL)
+clone = ptr;
+else
+{ return (malloc(new_size)); }
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr != NULL)
+{ free(ptr);
+return (0); }
+relloc = malloc(new_size);
+if (relloc == NULL)
+return (0);
+for (i = 0; i < (old_size || i < new_size); i++)
+{
+*(relloc + i) = clone[i];
+}
+free(ptr);
+return (relloc);
 }
