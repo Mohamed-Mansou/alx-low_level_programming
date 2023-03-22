83f25fac498c7142a298bfe1dfb1ed810670fd49
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-23 00:13:54 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,31 +1,75 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
- * get_op_func - ...
- * @s: ...
+ * op_add - ...
+ * @a: ...
+ * @b: ...
  *
  * Return: ...
  */
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div }
-	{ "%", op_mod },
-	{ NULL, NULL }
-	};
-	int i = 0;
+	return (a + b);
+}
 
-	while (i < 5)
+/**
+ * op_sub - ...
+ * @a: ...
+ * @b: ...
+ *
+ * Return: ...
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - ...
+ * @a: ...
+ * @b: ...
+ *
+ *  Return: ...
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - ...
+ * @a: ...
+ * @b: ...
+ *
+ *  Return: ...
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
-		i++;
+		printf("Error\n");
+		exit(100);
 	}
 
-	return (0);
+	return (a / b);
+}
+
+/**
+ * op_mod - ...
+ * @a: ...
+ * @b: ...
+ *
+ * Return: ...
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
 }
