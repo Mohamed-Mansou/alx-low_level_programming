cc7810f6789eb1b36d3a65590cd5ca22d718c955
Mohamed-Mansou <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-23 20:05:42 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,44 +8,43 @@
  */
 void print_all(const char * const format, ...)
 {
-  va_list list;
-  unsigned int j = 0, start = 0;
-  char *p;
-
-  va_start(list, format);
-  while (format && format[j] != '\0')
-    {
-      switch (format[j])
-	{ case 'c':
-	    switch (start)
-	      { case 1: printf(", "); }
-	    start = 1;
-	    printf("%c", va_arg(list, int));
-	    break;
-	case 'i':
-	  switch (start)
-	    { case 1: printf(", "); }
-	  start = 1;
-	  printf("%i", va_arg(list, int));
-	  break;
-	case 'f':
-	  switch (start)
-	    { case 1: printf(", "); }
-	  start = 1;
-	  printf("%f", va_arg(list, double));
-	  break;
-	case's':
-	  switch (start)
-	    { case 1: printf(", "); }
-	  start = 1;
-	  p = va_arg(list, char*);
-	  if (p)
-	    { printf("%s", p);
-	      break; }
-	  printf("%p", p);
-	  break; }
-      j++;
-    }
-  printf("\n");
-  va_end(list);
+va_list list;
+unsigned int j = 0, start = 0;
+char *p;
+va_start(list, format);
+while (format && format[j] != '\0')
+{
+switch (format[j])
+{ case 'c':
+switch (start)
+{ case 1: printf(", "); }
+start = 1;
+printf("%c", va_arg(list, int));
+break;
+case 'i':
+switch (start)
+{ case 1: printf(", "); }
+start = 1;
+printf("%i", va_arg(list, int));
+break;
+case 'f':
+switch (start)
+{ case 1: printf(", "); }
+start = 1;
+printf("%f", va_arg(list, double));
+break;
+case's':
+switch (start)
+{ case 1: printf(", "); }
+start = 1;
+p = va_arg(list, char*);
+if (p)
+{ printf("%s", p);
+break; }
+printf("%p", p);
+break; }
+j++;
+}
+printf("\n");
+va_end(list);
 }
