9ac1d06f121eafc3b0af00931d548a7e8069314f
Mohamed Mansour <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-03-31 01:46:53 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,19 +10,19 @@
 
 size_t print_list(const list_t *h)
 {
-  unsigned int size;
+unsigned int size;
 
-  size = 0;
+size = 0;
 
-  while (h)
-    {
-      if (h->str == NULL)
-	printf("[0] (nil)\n");
-      else
-	printf("[%i] %s\n", h->len, h->str);
-      h = h->next;
-      size += 1;
-    }
+while (h)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%i] %s\n", h->len, h->str);
+h = h->next;
+size += 1;
+}
 
-  return (size);
+return (size);
 }
