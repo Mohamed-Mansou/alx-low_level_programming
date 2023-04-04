fde94ce98c827f3be97a63e9fb623c5df8d3bd33
Mohamed Mansour <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-04-05 00:50:00 +0300

    task55

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,12 @@
  */
 void free_listint(listint_t *head)
 {
-listint_t *tmp;
-while (head != NULL)
-{
-tmp = head;
-head = head->next;
-free(tmp);
-}
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
 }
