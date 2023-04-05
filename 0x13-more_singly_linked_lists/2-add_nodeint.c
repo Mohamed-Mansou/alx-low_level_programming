3f38f6d228313b027d91a346eb4e632b84b4c51e
Mohamed Mansour <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-04-05 03:01:36 +0300

    taskkkkkkkkkkkkkkkkk

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,14 +9,12 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	
-	return (new);
+listint_t *new;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = *head;
+*head = new;
+return (new);
 }
