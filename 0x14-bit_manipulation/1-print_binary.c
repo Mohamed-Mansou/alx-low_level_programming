7b45b234c61a42683d9d1155aa69feb366b868ae
Mohamed Mansour <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-04-10 02:18:30 +0300

    taskkk

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
