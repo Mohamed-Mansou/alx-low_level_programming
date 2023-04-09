a4e2bc82e3edd32f47df2eda41298acdbef4b620
Mohamed Mansour <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-04-10 00:39:21 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,6 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
  * @letters: number of letters it should read and print
  *
  * Return: actual number of letters it could read and print
