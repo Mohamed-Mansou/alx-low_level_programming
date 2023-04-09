d13d0f2e8a832a00b595ef872aaeb890eecf2cd0
Mohamed Mansour <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-04-10 02:37:08 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,6 +3,7 @@
 /**
  * create_file - creates a file
  * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
@@ -17,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-	while (text_content[len])
+		while (text_content[len])
 			len++;
 		write(fd, text_content, len);
 	}
