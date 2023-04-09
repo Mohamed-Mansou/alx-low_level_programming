ebb1b777a7a1fa492d85b45c3c70bf66a68470c3
Mohamed Mansour <123316147+Mohamed-Mansou@users.noreply.github.com>
2023-04-10 00:37:23 +0300

    taskkkkkkkkk

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * Return: actual number of letters it could read and print
+ * 0 if the file can not be opened or read
+ * 0 if filename is NULL
+ * 0 if write fails or does not write the expected amount of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n_read, n_written;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	fdd = open(filename, O_RDONLY);
+	if (fdd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	n_read = read(fd, buf, letters);
+	if (n_read == -1)
+		return (0);
+	n_written = write(STDOUT_FILENO, buf, n_read);
+	if (n_written == -1)
+		return (0);
+	free(buf);
+	close(fd);
+	return (n_written);
+}
